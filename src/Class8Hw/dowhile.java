3342ce1e86be972595c700d6719d064806305136
Avadh@2009 <lightyear9461@gmail.com>
2021-12-10 23:38:00 +0000

    weeks

--- src/Class8Hw/dowhile.java
+++ src/Class8Hw/dowhile.java
@@ -9,3 +9,13 @@ public class dowhile {
 
     }while (variable<=10);
 }}
+class leapyear extends dowhile {
+    public static void main(String[] args) {
+        int year = 2020;
+        if(year%4==0){
+            System.out.println("2020 is leap year");
+        }
+        else {
+            System.out.println("2020 is not leap year");
+    }
+}}
