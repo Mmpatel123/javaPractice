7b7a68094a64f5e60b309627fa916432e2c1b413
Avadh@2009 <lightyear9461@gmail.com>
2021-12-11 00:20:05 +0000

    animal

--- src/Practicepackages/StringUse.java
+++ src/Practicepackages/StringUse.java
@@ -1,9 +1,15 @@
 package Practicepackages;
 
 
-public class practice2 {
+public class StringUse {
     public static void main(String[] args) {
-        String name = knowledge;
+        String name = "india";
+        System.out.println(name);
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.trim ());
+        System.out.println(name.charAt(2));
+        System.out.println(name.length());
+        System.out.println(name.indent(5) );
 
-    }
-}
+}}
